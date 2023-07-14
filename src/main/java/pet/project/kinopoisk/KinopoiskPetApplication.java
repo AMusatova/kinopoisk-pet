5980db7c3124dc2678f453d64585c5e189f620e9
AMusatova <49236545+AMusatova@users.noreply.github.com>
2023-07-14 16:30:19 +0300

    Initial commit

---
+++ src/main/java/pet/project/kinopoisk/KinopoiskPetApplication.java
@@ -0,0 +1,12 @@
+package pet.project.kinopoisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinopoiskPetApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KinopoiskPetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pet/project/kinopoisk/KinopoiskPetApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.project.kinopoisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinopoiskPetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
